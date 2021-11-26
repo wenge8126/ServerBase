f3f12ba991e1423da1f6c0aa3f5547f04e54a5e1
wenge8126 <moooy2@163.com>
2021-11-26 18:08:46 +0800

    TableActor 初始创建

--- BaseCommon/ActorDBLib/Code/TableActor.cpp
+++ BaseCommon/ActorDBLib/Code/TableActor.cpp
@@ -3,14 +3,37 @@
 #include "ActorManager.h"
 
 
-void NetCloud::TableActor::InitSQLTable()
+void NetCloud::TableActor::_AsyncInitDB(InitParam param)
+{
+	MySqlDBTool	mSql;
+	if (!mSql.InitStart(*param.mSQLParam))
+	{
+		ERROR_LOG("Init sql fail : %s", mSql.getErrorMsg());
+		return;
+	}
+
+	// 获取表信息: 名称, hash Key
+	AString tableName;
+	int hashKey = 0;
+	EActorTableType tableType = eATable_MYSQL;
+
+	// 创建
+	TableActorID tID;
+	tID.id = MAKE_INDEX_ID(tableName.c_str());
+	tID.hash = hashKey;
+	
+	Hand<TableActor> actor = param.mgr->CreateActor(TableActorType, tID.value);
+	actor->StartDB(tableType, param.mSQLParam);
+}
+
+void NetCloud::TableActor::InitSQLTable(AutoNice param)
 {
 	mDBTable = MEM_NEW MemoryDBTable(this);
 	Hand<SQLComponect> comp = AddComponent("MySQLComponect");
 	mDBTable->InitSQL(comp);
 }
 
-void NetCloud::TableActor::InitShareSQL()
+void NetCloud::TableActor::InitShareSQL(AutoNice param)
 {
 	mDBTable = MEM_NEW MemoryDBTable(this);
 	Hand<SQLComponect> comp = AddComponent("ShareSQLComponect");
@@ -18,7 +41,7 @@ void NetCloud::TableActor::InitShareSQL()
 }
 
 void NetCloud::TableActor::RegisterMsg(ActorManager *pActorMgr)
-{
+{	
 	REG_COMPONENT(pActorMgr, MySQLComponect);
 	REG_COMPONENT(pActorMgr, ShareSQLComponect);
 }
--- BaseCommon/ActorDBLib/Code/TableActor.h
+++ BaseCommon/ActorDBLib/Code/TableActor.h
@@ -3,9 +3,19 @@
 #include "MemoryDBTable.h"
 #include "Actor.h"
 #include "ActorDBLib.h"
+#include "ActorManager.h"
+
+#define TableActorType			(-10000)
 
 namespace NetCloud
 {
+	enum EActorTableType
+	{
+		eATable_MYSQL,
+		eATable_ShareSQL,
+		eATable_ShareMem,
+	};
+
 	class ActorDBLib_Export TableActor : public Actor
 	{
 	public:
@@ -15,13 +25,46 @@ namespace NetCloud
 		}
 
 	public:
+		struct InitParam
+		{
+			Auto<ActorManager> mgr;
+			AutoNice	mSQLParam;
+		};
+		static void InitDB(Auto<ActorManager> mgr, AutoNice sqlParam)
+		{
+			InitParam p = { mgr, sqlParam };
+			CoroutineTool::AsyncCall(&TableActor::_AsyncInitDB, p);
+		}
+
+		static void _AsyncInitDB(InitParam p);
+
+	public:
 		Auto<MemoryDBTable> GetTable() { return mDBTable; }
 
-		void InitSQLTable();
+		void StartDB(EActorTableType type, AutoNice param)
+		{
+			switch (type)
+			{
+			case NetCloud::eATable_MYSQL:
+				InitSQLTable(param);
+				break;
+			case NetCloud::eATable_ShareSQL:
+				InitShareSQL(param);
+				break;
+			case NetCloud::eATable_ShareMem:
+				InitShareTable(param);
+				break;
+			default:
+				ERROR_LOG("No exist type %d", type);
+				break;
+			}
+		}
 
-		void InitShareSQL();
+		void InitSQLTable(AutoNice param);
 
-		void InitShareTable()
+		void InitShareSQL(AutoNice param);
+
+		void InitShareTable(AutoNice param)
 		{
 			//mDBTable = ShareDBTable(this);
 		}
@@ -33,4 +76,18 @@ namespace NetCloud
 		Auto<MemoryDBTable> mDBTable;
 	};
 
+	//-------------------------------------------------------------------------
+	struct TableActorID
+	{
+		union
+		{
+			struct {
+				Int64				hash : 16;
+				Int64				type : 16;
+				Int64				id : 32;
+			};
+			UInt64			value;
+		};
+	};
+	//-------------------------------------------------------------------------
 }
